b6d6f172c4cfa1a618f82952cd6e6cca9ff9bc5d
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2024-11-21 21:23:27 +0530

    Updated elasicsearch

--- src/services/elasticSearch.ts
+++ src/services/elasticSearch.ts
@@ -2,8 +2,10 @@ const { Client } = require('@elastic/elasticsearch');
 
 // Elasticsearch client configuration
 const client = new Client({
-  node: 'http://elasticsearch_container:9200',  // Use the container name instead of localhost
-  requestTimeout: 10000 // 10 seconds timeout
+  node: 'https://my-elasticsearch-project-d0942f.es.us-east-1.aws.elastic.cloud:443',  // Use the elasticseach url
+  auth: {
+    apiKey: "NWhscVQ1TUJGdElsZ2FScFdkano6d1NTOWVIZEdSYXF4NXVjNWFoakdkUQ=="
+  }
 });
 
 export default client;
