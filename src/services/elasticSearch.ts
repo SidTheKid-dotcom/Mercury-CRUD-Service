157c54455f68aaed87c8f69edf07bae1274640bf
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2024-11-21 20:17:36 +0530

    Added ElasticSearch Timeout Limit

--- src/services/elasticSearch.ts
+++ src/services/elasticSearch.ts
@@ -3,6 +3,7 @@ const { Client } = require('@elastic/elasticsearch');
 // Elasticsearch client configuration
 const client = new Client({
   node: 'http://elasticsearch_container:9200',  // Use the container name instead of localhost
+  requestTimeout: 10000 // 10 seconds timeout
 });
 
 export default client;
