d7ee3584d874a521b4bed8c557ff3a07a70292ab
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2024-11-21 21:00:03 +0530

    Updated .gitignore

--- dist/services/elasticSearch.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const { Client } = require('@elastic/elasticsearch');
-// Elasticsearch client configuration
-const client = new Client({
-    node: 'http://elasticsearch_container:9200', // Use the container name instead of localhost
-});
-exports.default = client;
--- dist/services/queryAI.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateAIResponse = void 0;
-function generateAIResponse(query) {
-    // Simulate AI response generation
-    return `AI response for query: ${query}`;
-}
-exports.generateAIResponse = generateAIResponse;
