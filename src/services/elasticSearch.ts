b1389b38f1a707f4c83451c49f60ffd49ee4a049
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2024-12-09 15:56:13 +0530

    ElasticSearch Changed

--- src/services/elasticSearch.ts
+++ src/services/elasticSearch.ts
@@ -2,9 +2,9 @@ const { Client } = require('@elastic/elasticsearch');
 
 // Elasticsearch client configuration
 const client = new Client({
-  node: 'https://my-elasticsearch-project-d0942f.es.us-east-1.aws.elastic.cloud:443',  // Use the elasticseach url
+  node: 'https://e0c8499567524524a9201c165c9674d5.us-central1.gcp.cloud.es.io:443',  // Use the elasticseach url
   auth: {
-    apiKey: "NWhscVQ1TUJGdElsZ2FScFdkano6d1NTOWVIZEdSYXF4NXVjNWFoakdkUQ=="
+    apiKey: "bHdUeXFwTUIwUWNoZnhrLTN3OUs6TWN6N1lidEpUNmlxUUR6ekJ4OTBtUQ=="
   }
 });
 
