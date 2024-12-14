fe685b70f4d854ab63f0854aee90a424c0bb23a5
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2024-12-14 11:47:26 +0530

    Added Dept Analytics

--- src/routes/analyticsRoute.ts
+++ src/routes/analyticsRoute.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { getAnalytics } from "../controllers/analyticsController";
+import { getAnalytics, getDeptAnalytics } from "../controllers/analyticsController";
 
 const router = express.Router();
 
 // Route to handle folder uploads
 router.get("/", getAnalytics);
+
+router.get("/departments", getDeptAnalytics);
 
 export default router;
